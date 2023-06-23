cd8c52fe7d79df11097c78fa1aaa91ade94964e9
Nazim <nazim.messaoudene@pm.me>
2023-06-23 11:32:55 +0200

    test

---
+++ script.js
@@ -0,0 +1,10 @@
+//Dans le fichier script.js, écrivez une requête ciblant l’URL du backend hébergé sur Vercel pour récupérer l’année.
+
+fetch ('https://test-naz-backend.vercel.app/year')
+.then (response => response.json())
+.then (data => console.log(data))
+.then (data => {
+    let yearTextToAdd = document.querySelector("#year")
+yearTextToAdd.textContent = "cest la date du jour" + data.date
+
+})
